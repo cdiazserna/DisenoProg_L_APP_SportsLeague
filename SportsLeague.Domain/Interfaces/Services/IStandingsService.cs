a8070aa25b6b4063431e452b7831455bf936da58
Carlos Díaz <carlos.diaz@itsystemsinnova.com>
2026-05-04 19:34:59 -0500

    Phase 6 completed - Standings and Stats

---
+++ SportsLeague.Domain/Interfaces/Services/IStandingsService.cs
@@ -0,0 +1,15 @@
+﻿namespace SportsLeague.Domain.Interfaces.Services
+{
+    public interface IStandingsService
+    {
+        //keyword o palabra reservada "object"
+        //se utiliza para indicar que el método puede devolver
+        //cualquier tipo de dato. En este caso, se espera que el
+        //método devuelva un objeto que contenga la información de
+        //la tabla de posiciones, los máximos goleadores o las estadísticas de tarjetas,
+        //dependiendo del método específico.
+        Task<object> GetStandingsAsync(int tournamentId); // Obtener la tabla de posiciones para un torneo específico
+        Task<object> GetTopScorersAsync(int tournamentId); // Obtener la lista de los máximos goleadores para un torneo específico
+        Task<object> GetCardStatsAsync(int tournamentId); // Obtener las estadísticas de tarjetas para un torneo específico
+    }
+}
